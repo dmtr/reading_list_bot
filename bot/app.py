fd43935fa0debce0c599da65998d5bdad7499d68
Dmitriy Sorochenkov <podotkos@gmail.com>
2020-02-13 12:36:43 +0300

    Add peewee

--- bot/app.py
+++ bot/app.py
@@ -4,6 +4,7 @@ import os
 from typing import Dict
 from telegram.ext import Updater
 from bot.handlers import set_handlers
+from bot.db import get_db
 
 logger = logging.getLogger(__name__)
 
@@ -14,14 +15,15 @@ def get_config() -> Dict:
     }
 
 
-def create_updater():
+def create_updater() -> Updater:
     conf = get_config()
     updater = Updater(conf["token"], use_context=True)
     set_handlers(updater.dispatcher)
+    updater.dispatcher.db = get_db()
     return updater
 
 
-def run():
+def run() -> None:
     logger.info('Starting up')
     u = create_updater()
     u.start_polling()
--- bot/db.py
+++ bot/db.py
@@ -1,12 +1,107 @@
 import os
+from typing import Any, Dict, Tuple
+
+from peewee import (
+    SQL,
+    BigIntegerField,
+    CharField,
+    DateTimeField,
+    ForeignKeyField,
+    IntegerField,
+    Model,
+    TextField,
+)
+from playhouse.pool import PooledPostgresqlExtDatabase
+
+MAX_CONNECTIONS = 10
+STALE_TIMEOUT = 300
+
+ARTICLE_STATUSES = ("NEW", "READ")
+
+db = PooledPostgresqlExtDatabase(None)
+
+
+def get_connection_params() -> Dict:
+    return {
+        "host": os.environ.get("POSTGRES_HOST"),
+        "port": int(os.environ.get("POSTGRES_PORT")),
+        "user": os.environ.get("POSTGRES_USER"),
+        "password": os.environ.get("POSTGRES_PASSWORD"),
+        "dbname": os.environ.get("POSTGRES_DBNAME"),
+    }
 
 
 def get_connection_url() -> str:
-    host = os.environ.get("POSTGRES_HOST")
-    port = int(os.environ.get("POSTGRES_PORT"))
-    user = os.environ.get("POSTGRES_USER")
-    password = os.environ.get("POSTGRES_PASSWORD")
-    dbname = os.environ.get("POSTGRES_DBNAME")
-
-    url = "postgresql://{}:{}@{}:{}/{}".format(user, password, host, port, dbname)
+    params = get_connection_params()
+    url = f"postgresql://{params['user']}:{params['password']}@{params['host']}:{params['port']}/{params['dbname']}"
     return url
+
+
+def get_db() -> PooledPostgresqlExtDatabase:
+    params = get_connection_params()
+    dbname = params.pop("dbname")
+    db.init(
+        dbname, max_connections=MAX_CONNECTIONS, stale_timeout=STALE_TIMEOUT, **params
+    )
+    return db
+
+
+class EnumField(CharField):
+    def __init__(self, choices: Tuple, *args: Any, **kwargs: Any) -> None:
+        super(CharField, self).__init__(*args, **kwargs)
+        self.choices = choices
+        self.max_length = 255
+
+    def adapt(self, value: Any) -> Any:
+        if value not in self.choices:
+            raise TypeError("Wrong choice")
+
+        return value
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Article(BaseModel):
+    created_at = DateTimeField(constraints=[SQL("DEFAULT now()")], index=True)
+    status = EnumField(ARTICLE_STATUSES)
+    text = TextField()
+
+    class Meta:
+        table_name = "article"
+
+
+class TelegramUser(BaseModel):
+    created_at = DateTimeField(constraints=[SQL("DEFAULT now()")])
+    first_name = CharField()
+    telegram_id = BigIntegerField(unique=True)
+
+    class Meta:
+        table_name = "telegram_user"
+
+
+class UserArticleM2M(BaseModel):
+    article = ForeignKeyField(
+        column_name="article_id", field="id", model=Article, null=True
+    )
+    user = ForeignKeyField(
+        column_name="user_id", field="id", model=TelegramUser, null=True
+    )
+
+    class Meta:
+        table_name = "user_article_m2m"
+        primary_key = False
+
+
+class UserSettings(BaseModel):
+    article_ttl_in_days = IntegerField()
+    reading_list_size = IntegerField()
+    user = ForeignKeyField(
+        column_name="user_id", field="id", model=TelegramUser, null=True
+    )
+
+    class Meta:
+        table_name = "user_settings"
+        primary_key = False
